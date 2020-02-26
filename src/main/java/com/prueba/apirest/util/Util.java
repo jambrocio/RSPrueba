898dd8a51bc78e6ffe64be1d37f2dd92c5203861
jambrocio <59935300+consultorDeveloper3@users.noreply.github.com>
2020-02-26 09:21:44 -0500

    properties
    se elimino informacion que no se utiliza del properties

--- src/main/java/com/prueba/apirest/util/Util.java
+++ src/main/java/com/prueba/apirest/util/Util.java
@@ -1,47 +1,10 @@
 package com.prueba.apirest.util;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-
-import org.apache.http.HttpResponse;
-import org.apache.http.NameValuePair;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.HttpClientBuilder;
-import org.apache.http.message.BasicNameValuePair;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Util {
 	
-	public static String wsUrl;
 	
-	@Value("${websocket.url}")
-	public void setParamSocket(String url) {
-		wsUrl = url;
-	}
-	
-	public static void ejecutarWS() {
-		
-		HttpClient client = HttpClientBuilder.create().build();
-        HttpPost post = new HttpPost(wsUrl);
-        
-        List<NameValuePair> arguments = new ArrayList<>(1);
-        arguments.add(new BasicNameValuePair("channel", "reload-atenciones"));
-
-        try {
-            post.setEntity(new UrlEncodedFormEntity(arguments));
-            HttpResponse response = client.execute(post);
-
-            // Print out the response message
-            //System.out.println(EntityUtils.toString(response.getEntity()));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-	
-	}
 	
 }
